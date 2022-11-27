a9cb865a34a6d7c79f51212abd51fb55b357ea58
PurpleCloudy <forstudy546@gmail.com>
2022-11-27 13:43:52 +0300

    news

---
+++ first_app/news/forms.py
@@ -0,0 +1,28 @@
+from dataclasses import field
+from django import forms
+from .models import News, Comments
+
+# class NewsForm(forms.Form):
+#     article = forms.CharField()
+#     body = forms.CharField(widget=forms.Textarea)
+
+class NewsModelForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['article',
+                  'body',
+                  'image'
+                ]
+    
+    def clean_article(self):
+        data = self.cleaned_data.get('article')
+        if len(data) < 5:
+            raise forms.ValidationError('article is not long enough')
+        return data
+
+class CommentaryModelForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = [
+            'text'
+        ]
